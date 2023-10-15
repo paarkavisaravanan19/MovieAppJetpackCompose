01e2aec18943657c58d52bd37ec06dfb29aa4fa3
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-10-15 16:56:26 +0530

    [UPDATE] Surface is updated with movieViewModel

--- app/src/main/java/com/example/movieappapi/MainActivity.kt
+++ app/src/main/java/com/example/movieappapi/MainActivity.kt
@@ -3,25 +3,34 @@ package com.example.movieappapi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.movieappapi.model.Movie
 import com.example.movieappapi.ui.theme.MovieAppAPITheme
+import com.example.movieappapi.view.MovieItem
+import com.example.movieappapi.viewModel.MovieViewModel
 
 class MainActivity : ComponentActivity() {
+    val movieViewModel by viewModels<MovieViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppAPITheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    MovieList(movieList = movieViewModel.movieListResponse)
+                    movieViewModel.getMovieList()
 
                 }
             }
@@ -31,10 +40,27 @@ class MainActivity : ComponentActivity() {
 
 
 
+@Composable
+fun MovieList(movieList: List<Movie>) {
+    LazyColumn {
+        itemsIndexed(items = movieList) { index, item ->
+            MovieItem(movie = item)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     MovieAppAPITheme {
+        val movie = Movie(
+            "Coco",
+            "https://howtodoandroid.com/images/coco.jpg",
+            "Coco is a 2017 American 3D computer-animated musical fantasy adventure film produced by Pixar",
+            "Latest"
+        )
+
+        MovieItem(movie = movie )
 
     }
 }
