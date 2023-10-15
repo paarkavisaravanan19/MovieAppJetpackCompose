7ee13d636330c7401ac8135faceaed11382dbc74
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-10-15 16:54:41 +0530

    [UPDATE] UI of the HomeScreen updated

---
+++ app/src/main/java/com/example/movieappapi/view/HomeScreenUI.kt
@@ -0,0 +1,100 @@
+package com.example.movieappapi.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.base.R
+import coil.compose.rememberImagePainter
+import coil.size.Scale
+import coil.transform.CircleCropTransformation
+import com.example.movieappapi.model.Movie
+
+@Composable
+fun MovieItem(movie: Movie) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp, 4.dp)
+            .fillMaxWidth()
+            .height(110.dp), shape = RoundedCornerShape(8.dp),
+    ){
+        Surface() {
+
+            Row(
+                Modifier
+                    .padding(4.dp)
+                    .fillMaxSize()
+            ) {
+
+                Image(
+                    painter = rememberImagePainter(
+                        data = movie.imageUrl,
+
+                        builder = {
+                            scale(Scale.FILL)
+                            placeholder(coil.compose.base.R.drawable.notification_action_background)
+                            transformations(CircleCropTransformation())
+
+                        }
+                    ),
+                    contentDescription = movie.desc,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.2f)
+                )
+
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxHeight()
+                        .weight(0.8f)
+                ) {
+                    Text(
+                        text = movie.name,
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = movie.category,
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier
+                            .background(
+                                Color.LightGray
+                            )
+                            .padding(4.dp)
+                    )
+                    Text(
+                        text = movie.desc,
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                }
+            }
+        }
+    }
+
+}
